55a766986a5282bd58293f57827f88718cbd3b40
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-04-25 13:28:46 +0300

    start point

---
+++ generate.sh
@@ -0,0 +1,166 @@
+#!/bin/sh -x
+
+# disk image size (in megabytes)
+DISKSIZE=1024
+ROOT_PASSWORD=root
+
+# some utils (zypper) needs an absolute paths only
+TMPDIR=`pwd`/tmp
+ROOTFS="$TMPDIR"/rootfs
+IMG="$TMPDIR"/image.raw
+
+set -e
+
+## ---------------------------------------------
+## cleaning...
+if [ -d "$ROOTFS" ]; then
+    umount "$ROOTFS"/dev || :
+    chroot "$ROOTFS" umount -a || :
+    umount -f "$ROOTFS" || :
+    rmdir "$ROOTFS"
+fi
+if [ -f $IMG ]; then
+    LOOPDEVS=`losetup --associated "$IMG" | sed -E 's/^([^:]+).*$/\1/'`
+    for LD in $LOOPDEVS; do
+        kpartx -d "$LD" || :
+        losetup --detach "$LD"
+    done
+    rm -f -- "$IMG"
+fi
+
+## ---------------------------------------------
+## create disk image and mount it...
+qemu-img create -f raw "$IMG" ${DISKSIZE}M
+LOOPDEV=`losetup --find`
+losetup "$LOOPDEV" "$IMG"
+parted --script "$LOOPDEV" mklabel msdos
+parted --script "$LOOPDEV" mkpart primary ext2 0 $DISKSIZE
+kpartx -a "$LOOPDEV"
+LOOPDEV1=/dev/mapper/`basename "$LOOPDEV"`p1
+mkfs.ext3 "$LOOPDEV1"
+mkdir -m 755 -p "$ROOTFS"
+mount "$LOOPDEV1" "$ROOTFS"
+
+## ---------------------------------------------
+## initiating root fs directory...
+mkdir -m 755 -p "$ROOTFS"/dev
+mknod -m 666 "$ROOTFS"/dev/null c 1 3
+mknod -m 666 "$ROOTFS"/dev/zero c 1 5
+
+## ---------------------------------------------
+## register package repos...
+for URL in `cat repo.list`; do
+    zypper \
+        --root "$ROOTFS" \
+        addrepo \
+        --refresh \
+        "$URL" "$URL"
+done
+
+## ---------------------------------------------
+## installing packages...
+zypper \
+    --root "$ROOTFS" \
+    --gpg-auto-import-keys \
+    --no-gpg-checks \
+    --non-interactive \
+    install \
+    --name \
+    --download-in-advance \
+    --auto-agree-with-licenses \
+    --no-recommends \
+    -- \
+    aaa_base sysvinit util-linux lilo kernel-default-base perl openssh \
+    less vim pciutils iputils \
+    `cat package.list`
+
+## ---------------------------------------------
+## generate ssh key for access to target...
+if [ ! -f .ssh/id_rsa ]; then
+    mkdir -m755 -p .ssh
+    ssh-keygen -t rsa -N "" -q -f .ssh/id_rsa
+fi
+if [ ! -f .ssh/config ]; then
+    cat > .ssh/config <<-EOF
+	IdentityFile .ssh/id_rsa
+	UserKnownHostsFile .ssh/known_hosts
+	StrictHostKeyChecking no
+	Host *
+	    User root
+	EOF
+fi
+
+## ---------------------------------------------
+## configuring system...
+cat > "$ROOTFS"/etc/fstab << EOF
+proc    /proc     proc    nosuid,noexec,gid=proc 0 0
+sysfs   /sys      sysfs   defaults 0 0
+devpts  /dev/pts  devpts  mode=0620,gid=5 0 0
+EOF
+# get disk geometry...
+REGEXP='^([0-9]+) heads, ([0-9]+) sectors/track, ([0-9]+) cylinders, .*$'
+GEOMETRY=`fdisk -l "$LOOPDEV" | grep -E "$REGEXP" | sed -r "s@$REGEXP@\1 \2 \3@"`
+set $GEOMETRY
+cat > "$ROOTFS"/etc/lilo-loop.conf << EOF
+boot=$LOOPDEV
+disk=$LOOPDEV
+  bios=0x80
+  heads=$1
+  sectors=$2
+  cylinders=$3
+  partition=$LOOPDEV1
+  start=63
+delay=1
+vga=0
+image=/boot/vmlinuz
+  initrd=/boot/initrd
+  append="root=/dev/sda1"
+  label=Linux
+EOF
+cat > "$ROOTFS"/etc/sysconfig/bootloader << EOF
+LOADER_TYPE=none
+LOADER_LOCATION=none
+EOF
+cat > "$ROOTFS"/etc/sysconfig/network/ifcfg-eth0 << EOF
+BOOTPROTO='dhcp'
+MTU=''
+REMOTE_IPADDR=''
+STARTMODE='onboot'
+EOF
+mkdir -p "$ROOTFS"/root/.ssh
+ssh-keygen -t rsa -N "" -q -f "$ROOTFS"/root/.ssh/id_rsa
+ssh-keygen -y -q -f .ssh/id_rsa > "$ROOTFS"/root/.ssh/authorized_keys
+# to jump between nodes without password:
+ssh-keygen -y -q -f "$ROOTFS"/root/.ssh/id_rsa >> "$ROOTFS"/root/.ssh/authorized_keys
+chroot "$ROOTFS" chkconfig --add sshd
+
+## ---------------------------------------------
+## creating initrd...
+mount /dev "$ROOTFS"/dev -o bind
+# we need mounted /dev to change root password
+chroot "$ROOTFS" sh -c "echo '$ROOT_PASSWORD' | passwd --stdin"
+chroot "$ROOTFS" mount /proc
+chroot "$ROOTFS" mkinitrd -d "$LOOPDEV1" -f block
+chroot "$ROOTFS" lilo -v -C /etc/lilo-loop.conf
+chroot "$ROOTFS" umount /proc
+umount "$ROOTFS"/dev
+
+## ---------------------------------------------
+## clean image...
+## ---------------------------------------------
+## uninstalling packages...
+#zypper \
+#    --root "$ROOTFS" \
+rm -rf -- "$ROOTFS"/var/cache/zypp
+
+## ---------------------------------------------
+## umounting target...
+umount "$ROOTFS"
+kpartx -d "$LOOPDEV"
+losetup --detach "$LOOPDEV"
+rmdir "$ROOTFS"
+
+## ---------------------------------------------
+## success
+echo "Done"
+
